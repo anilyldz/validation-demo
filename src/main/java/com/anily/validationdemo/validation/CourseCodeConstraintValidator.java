937d03b1dc033a6f5fb889bdc82d5ec954bd8af1
anilyildiz <anil-yildiz@outlook.com.tr>
2024-08-04 13:29:51 +0300

    Custom validation for CourseCode is added

---
+++ src/main/java/com/anily/validationdemo/validation/CourseCodeConstraintValidator.java
@@ -0,0 +1,23 @@
+package com.anily.validationdemo.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class CourseCodeConstraintValidator implements ConstraintValidator<CourseCode, String> {
+
+    private  String coursePrefix;
+
+    @Override
+    public void initialize(CourseCode courseCode) {
+        coursePrefix = courseCode.value();
+    }
+
+    @Override
+    public boolean isValid(String code, ConstraintValidatorContext constraintValidatorContext) {
+        if (code != null) {
+            return code.startsWith(coursePrefix);
+        } else {
+            return false;
+        }
+    }
+}
